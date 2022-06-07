ea21e7ff61098ff3fee4470611f2bb50dc57536f
MargothAmz <93557612+MargothAmz@users.noreply.github.com>
2022-06-07 15:54:13 -0500

    Add files via upload

---
+++ MPRO-C25/main.js
@@ -0,0 +1,16 @@
+//Encuntra el error 
+var playerPaddle, computerPaddle, ball,
+
+function setup(){
+    // Agrega el código para crear los sprites, el area de trabajo. 
+
+
+  
+  
+}
+
+function draw(){
+  background("black");
+   // Llama a la función draw debajo
+  
+}
